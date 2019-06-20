1e32833cc7028983285122c2003541aded2f8d2e
Bruno <hey@brunoandrade.me>
2019-06-20 01:30:44 -0300

    (add) global styles

--- src/styles/global.js
+++ src/styles/global.js
@@ -2,14 +2,32 @@ import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
   * {
-    padding: 0;
     margin: 0;
-    outline: 0;
+    padding: 0;
     box-sizing: border-box;
+    outline: 0;
   }
-  
+  *:focus {
+    outline: 0;
+  }
   html, body, #root {
-    height: 100%;
+    min-height: 100%;
     background-color: ${({ theme }) => theme.COLORS.PRIMARY};
   }
+  body {
+    text-rendering: optimizeLegibility !important;
+    -webkit-font-smoothing: antialiased !important;
+    font-family: 'Source Sans Pro', sans-serif;
+  }
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+  ul {
+    list-style: none;
+  }
+  button {
+    cursor: pointer;
+  }
+
 `;
