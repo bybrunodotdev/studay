2c0a87ac74ab6fa5445a88206419b69dfa2b881f
Bruno <hey@brunoandrade.me>
2019-06-20 15:52:55 -0300

    (add) config store

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
 
 import Signin from '~/pages/Auth/Signin';
@@ -6,13 +7,17 @@ import Signin from '~/pages/Auth/Signin';
 import GlobalStyle from '~/styles/global';
 import theme from '~/styles/theme';
 
+import store from './store';
+
 const App = () => (
-  <ThemeProvider theme={theme}>
-    <>
-      <Signin />
-      <GlobalStyle />
-    </>
-  </ThemeProvider>
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <>
+        <Signin />
+        <GlobalStyle />
+      </>
+    </ThemeProvider>
+  </Provider>
 );
 
 export default App;
---
+++ src/store/index.js
@@ -0,0 +1,29 @@
+import { createStore, compose, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { routerMiddleware } from 'connected-react-router';
+
+import history from '~/history';
+import rootReducer from './ducks';
+import rootSaga from './sagas';
+
+const sagaMiddleware = createSagaMiddleware();
+const middlewares = [sagaMiddleware, routerMiddleware(history)];
+
+/**
+ * Configuration redux-devtools-extension
+ */
+
+/* eslint-disable */
+const composeSetup =
+  process.env.NODE_ENV !== 'production' &&
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : compose;
+/* eslint-anable */
+
+const store = createStore(rootReducer(history), composeSetup(applyMiddleware(...middlewares)));
+
+sagaMiddleware.run(rootSaga);
+
+export default store;
