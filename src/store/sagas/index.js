98bb4a4166d4ca8da7490561fe0d939864a01158
Bruno <hey@brunoandrade.me>
2019-06-22 05:26:38 -0300

    (remove): unnecessary import removal

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,7 +1,6 @@
-import { all, takeLatest } from 'redux-saga/effects';
+import { all } from 'redux-saga/effects';
 
 import { userSagas } from './auth';
-import { AuthTypes } from '../ducks/auth';
 
 export default function* rootSaga() {
   yield all([...userSagas]);
